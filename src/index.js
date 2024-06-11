e625f7f21f897e6d305991b0e220d4dc07660a29
praveenprk <praveenkumarkp666@gmail.com>
2024-06-12 01:00:55 +0530

    setup server for id reconcliation task: - expose identify endpoint

--- src/index.js
+++ src/index.js
@@ -1,21 +1,41 @@
 const http = require('http');
+const url = require('url');
+const { identifyHandler } = require('./identify');
+
+const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-    
-    if (req.url === '/') {
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify({
-            data: 'Bite Speed - Backend Task Identity Reconciliation!',
-        }));
-        return;
-    }
 
-    if (req.url === '/identify') {
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.write(JSON.stringify({data: 'identify api'}));
-        res.end();
-        return;
-    }
-  });
-  
-  server.listen(8000);
+  if (req.url === '/') {
+    res.write(JSON.stringify({
+        company: 'BiteSpeed',
+        task_type: 'Backend',
+        desc: 'Identity Reconcilation Project'
+    }));
+    res.end();
+    return;
+  }
+
+  if (req.method === 'POST' && req.url === '/identify') {
+    let details = '';
+    req.on('data', chunk => {
+      details += chunk.toString();
+    });
+
+    req.on('end', () => {
+      const data = JSON.parse(details);
+      console.log(`data from index:`, data);
+      return;
+    });
+    
+  } else {
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({ message: 'Not Found' }));
+    return;
+  }
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
